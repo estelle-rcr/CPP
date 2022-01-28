3b2692efc130103a57673f4b2062156e6177cae0
Estelle Recuero <estelle-ad@outlook.com>
2022-01-28 17:14:06 +0100

    in progress

---
+++ cpp00/ex01/includes/PhoneBook.hpp
@@ -0,0 +1,42 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   PhoneBook.hpp                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: erecuero <erecuero@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/01/06 18:28:49 by erecuero          #+#    #+#             */
+/*   Updated: 2022/01/28 13:42:50 by erecuero         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PHONEBOOK_CLASS_H
+# define PHONEBOOK_CLASS_H
+
+#include <iostream>
+#include <stdio.h>
+#include <ctype.h>
+#include <string.h>
+#include <iomanip>
+#include "Contact.hpp"
+
+#define NB_CONTACTS 8
+
+class PhoneBook {
+
+public:
+	std::string	attributes[5];
+
+	PhoneBook(std::string a1, std::string a2, std::string a3, std::string a4, std::string a5);
+	~PhoneBook();
+
+	bool	addContact( void );
+	bool	searchContact( void ) const;
+	bool	displayContact( int index ) const;
+
+private:
+	int		_index;
+	Contact	_contacts[NB_CONTACTS];
+};
+
+#endif
---
+++ cpp00/ex01/srcs/main.cpp
@@ -0,0 +1,99 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: erecuero <erecuero@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/01/26 15:44:01 by erecuero          #+#    #+#             */
+/*   Updated: 2022/01/28 17:00:20 by erecuero         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "PhoneBook.hpp"
+
+void	intro( void ) {
+
+	std::cout << "\n> Please select one of the following functions: " << std::endl;
+	std::cout << "-> enter 'ADD' for adding a new contact" << std::endl;
+	std::cout << "-> enter 'SEARCH' for displaying a contact information" << std::endl;
+	std::cout << "-> enter 'EXIT' for existing (you'll lose all the contacts)" << std::endl;
+}
+/*
+std::string	getInput( void ) {
+
+	std::string	buffer;
+
+	if (!getline(std::cin, buffer, '\n')) {
+		std::cout << "error in input\n";
+		return NULL;
+	}
+	else if (std::cin.eof() == 1) {
+		std::cout << "error with end-of-file\n";
+		return NULL;
+	}
+	return buffer;
+}*/
+
+void	searchFunBook( PhoneBook funbook ) {
+
+	std::string	input;
+	int			index;
+	std::string::size_type sz;   // alias of size_t
+
+	if (funbook.searchContact() == false)
+		std::cout << "\nNo contact to be displayed yet.\n" << std::endl;
+	else {
+		std::cout << "> Select an index: " << std::endl;
+		if (!getline(std::cin, input, '\n')) {
+			std::cout << "error in input\n";
+			return ;
+		}
+		index = std::stoi(input, &sz);
+		while (index == 0) {
+			if (!getline(std::cin, input, '\n')) {
+				std::cout << "error in input\n";
+				return ;
+			}
+			index = std::stoi(input, &sz);
+		}
+		funbook.displayContact(index);
+	}
+	return ;
+}
+
+int	main( void ) {
+
+	PhoneBook funbook("Last Name", "First Name", "Nick Name", "Phone Number", "Darkest secret");
+	std::string	input;
+
+	std::cout << "Welcome in your FunBook!" << std::endl;
+	intro();
+	if (!getline(std::cin, input, '\n')) {
+		std::cout << "\nerror in input\n" << std::endl;
+		return 1;
+	}
+	while (input != "EXIT") {
+		if (input.compare("ADD") == 0)
+		{
+			if (funbook.addContact() == false) {
+				std::cout << "\nerror in adding a contact" << std::endl;
+				return 1;
+			}
+		}
+		else if (input.compare("SEARCH") == 0)
+			searchFunBook(funbook);
+		intro();
+		if (!getline(std::cin, input, '\n')) {
+			std::cout << "\nerror in input\n" << std::endl;
+			return 1;
+		}
+	}
+	return 0;
+}
+
+//		"Last name"
+//	"First name"
+//	"Nick name"
+//	"Phone Number"
+//	"Darkest secret"
