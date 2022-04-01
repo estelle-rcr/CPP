c6c93da784db78d305876cbbb6956b6b7a7e12cd
Estelle Recuero <estelle-ad@outlook.com>
2022-04-01 20:17:50 +0200

    correct

--- cpp08/ex02/srcs/main.cpp
+++ cpp08/ex02/srcs/main.cpp
@@ -6,7 +6,12 @@
 /*   By: erecuero <erecuero@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/01 20:05:48 by erecuero          #+#    #+#             */
-/*   Updated: 2022/04/01 20:05:49 by erecuero         ###   ########.fr       */
+/*   Updated: 2022/04/01 20:17:37 by erecuero         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+# include "MutantStack.hpp"
+
+int main(void) {
+
+}
